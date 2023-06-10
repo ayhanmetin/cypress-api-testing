828377836f3f31038cb17513a03ff7a87802abda
Ayhan Metin <ayhanmet1@gmail.com>
2023-06-10 12:26:01 +0200

    api chaining

---
+++ apiChaining.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe('api chaining', () => {
+  
+  it('getting all the post', () => {
+    
+    cy.request({
+      method: 'GET',
+      url: 'https://jsonplaceholder.typicode.com/posts'
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      const postId = response.body[0].id
+      return postId;
+    }).then((postId)=>{
+      cy.request({
+        method: 'GET',
+        url: `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+      }).then((response) =>{
+        expect(response.status).to.eq(200)
+        expect(response.body).to.have.length(5)
+      })
+    })
+  });
+
+})
